9c0acc82ef9c9b051349499c31ac091aa61a19a7
Marcel <mvelasquez2106@gmail.com>
2026-02-15 20:58:43 +0100

    Rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,19 @@ int main()
 
     float vertices[] =
     {
-        0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-        0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f
     };
+    
+    unsigned int indices[] =
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    Shader shader("src/shader/vertex.vs", "src/shader/fragment.fs");
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -33,7 +42,11 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    Shader shader("src/shader/vertex.vs", "src/shader/fragment.fs");
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -49,7 +62,8 @@ int main()
         glClearColor(0.0, 0.0, 0.2, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         window.pollEvents();
         window.swapBuffer();
